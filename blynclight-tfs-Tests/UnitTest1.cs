e2560fd189820c240a16355eda2f799a49c9ebbe
azam alias <azamalias@gmail.com>
2015-11-13 23:13:12 +0800

    added nunit test project and package

---
+++ blynclight-tfs-Tests/UnitTest1.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace blynclight_tfs_Tests
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
