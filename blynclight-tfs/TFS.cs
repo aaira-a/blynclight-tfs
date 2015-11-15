37fab788a7a2ace5df1eda501de36d36f0009d9a
azam alias <azamalias@gmail.com>
2015-11-15 23:21:44 +0800

    queried specific work items from example (more)
    from https://msdn.microsoft.com/en-us/library/bb130306%28v=vs.120%29.aspx

--- blynclight-tfs/TFS.cs
+++ blynclight-tfs/TFS.cs
@@ -3,6 +3,7 @@ using System.Collections.ObjectModel;
 using Microsoft.TeamFoundation.Client;
 using Microsoft.TeamFoundation.Framework.Common;
 using Microsoft.TeamFoundation.Framework.Client;
+using Microsoft.TeamFoundation.WorkItemTracking.Client;
 
 namespace TfsApplication
 {
@@ -10,42 +11,23 @@ namespace TfsApplication
     {
         static void Main(String[] args)
         {
-            // Connect to Team Foundation Server
-            //     Server is the name of the server that is running the application tier for Team Foundation.
-            //     Port is the port that Team Foundation uses. The default port is 8080.
-            //     VDir is the virtual path to the Team Foundation application. The default path is tfs.
-            Uri tfsUri = (args.Length < 1) ?
-                new Uri("http://Server:Port/VDir") : new Uri(args[0]);
+            // Connect to the work item store
+            TfsTeamProjectCollection tpc =
+                new TfsTeamProjectCollection(
+                    new Uri(args[0]));
+                
+            WorkItemStore workItemStore = (WorkItemStore)tpc.GetService(typeof(WorkItemStore));
 
-            TfsConfigurationServer configurationServer =
-                TfsConfigurationServerFactory.GetConfigurationServer(tfsUri);
+            // Run a query.
+            WorkItemCollection queryResults = workItemStore.Query(
+               "Select [ID], [Title] " +
+               "From WorkItems " +
+               "Where [Work Item Type] = 'Support' " +
+               "AND [State] = 'New' " +
+               "AND [Area Path] = 'Nintex\\NW for O365' " +
+               "Order By [ID] Asc");
 
-            // Get the catalog of team project collections
-            ReadOnlyCollection<CatalogNode> collectionNodes = configurationServer.CatalogNode.QueryChildren(
-                new[] { CatalogResourceTypes.ProjectCollection },
-                false, CatalogQueryOptions.None);
-
-            // List the team project collections
-            foreach (CatalogNode collectionNode in collectionNodes)
-            {
-                // Use the InstanceId property to get the team project collection
-                Guid collectionId = new Guid(collectionNode.Resource.Properties["InstanceId"]);
-                TfsTeamProjectCollection teamProjectCollection = configurationServer.GetTeamProjectCollection(collectionId);
-
-                // Print the name of the team project collection
-                Console.WriteLine("Collection: " + teamProjectCollection.Name);
-
-                // Get a catalog of team projects for the collection
-                ReadOnlyCollection<CatalogNode> projectNodes = collectionNode.QueryChildren(
-                    new[] { CatalogResourceTypes.TeamProject },
-                    false, CatalogQueryOptions.None);
-
-                // List the team projects in the collection
-                foreach (CatalogNode projectNode in projectNodes)
-                {
-                    Console.WriteLine(" Team Project: " + projectNode.Resource.DisplayName);
-                }
-            }
+            Console.WriteLine(queryResults.Count);
         }
     }
 }
